f4b98578fc610c87dabd15ec4d0e7d28f2aba9a0
mis0butter <junetter@gmail.com>
2024-02-06 18:35:39 -0600

    committing init stuff, dl'd ode78rpr and created gitignore

---
+++ HW1/HW1_part1_KS_formulation.m
@@ -0,0 +1,12 @@
+%% PART 1: KS FORMULATION 
+
+init 
+
+%% PART 1.1: UNPERTURBED 
+
+% part a: Code the transformation subroutines for transformations in both 
+% directions from { u, u' } <--> { r, v }. Give a screenshot of the 
+% subroutines. (separate from providing src in the appendix)
+
+
+
