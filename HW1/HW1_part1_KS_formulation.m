0abb7aff6a31fda54390088997d60372d9d760ab
Junette Hsin <junetter@gmail.com>
2024-02-11 04:46:56 -0600

    ok, I did a_VTN correctly. Matches Russell's slides. ugh took so long

--- HW1/HW1_part1_KS_formulation.m
+++ HW1/HW1_part1_KS_formulation.m
@@ -118,8 +118,7 @@ sprintf( "T = %.10g s", T )
 % ii: Plot the Cartesian trajectory, with clear markers at the nodes and 
 % lines connecting them. Use >>view(30,30) to see the 3D component. 
 
-figure(1) ; 
-    plot_rv(rv_hist, 'Analytical Cartesisan Trajectory') ; 
+plot_rv(rv_hist, 'Analytical Cartesian Trajectory') ; 
     
     
 % ----------------------- % 
@@ -262,6 +261,48 @@ eps_prime   = state_prime(end)
 % 0.03 LU/TU2, longitude=60 deg, latitude=30 deg. Propagate for 4 periods 
 % (using the IC’s assuming ballistic motion) and 120 total equal steps in 
 % tau. Use the same fixed step integrator.
+
+% a in VTN frame 
+Tmag  = 0.005 ; 
+lon   = 0 * pi/180 ; 
+lat   = 90 * pi/180 ; 
+a_VTN = latlon2VTN( Tmag, lat, lon ) ; 
+
+a       = 1 ;             % LU 
+e       = 0.7 ; 
+i       = 40 * pi/180;      % deg
+w       = 0 ;  % deg 
+Omega   = 0 ;  % deg 
+nu      = 0 ;  % deg 
+mu      = 1 ;            % LU^3 / TU^2 
+
+% orbital elements and cartesian 
+oe0     = [ a; e; i; w; Omega; nu ] ; 
+rv0     = oe2rv( oe0, mu ) ; 
+
+r0 = rv0(1:3) ; 
+v0 = rv0(4:6) ; 
+
+% { u, u' } <--> { r, v } 
+KS0 = rv2KS( rv0 ) ; 
+rv  = KS2rv( KS0 ) ;    % check 
+
+u0      = KS0(1:4) ; 
+uprime0 = KS0(5:8) ; 
+t0      = 0 ; 
+eps0    = 2 * dot( uprime0, uprime0 ) / ( dot( u0, u0 ) ) - mu / ( dot( u0, u0 ) ) ; 
+
+% period in time (s) 
+T = 2*pi*sqrt( a^3 / mu ) ; 
+    
+% period in tau (tau units) 
+k = 1 ; 
+T_tau = T / ( k * a ) ; 
+
+state0  = [ u0 ; uprime0 ; t0 ; eps0 ] ; 
+
+[ state_num, rv_num ] = prop_KS_num( state0, a_VTN, mu, T_tau * 10, N * 10, tol ) ; 
+plot_rv( rv_num ) ; 
 
 
 
--- HW1/fns/KS_EOM.m
+++ HW1/fns/KS_EOM.m
@@ -1,20 +1,33 @@
-function KS_t_eps_prime = KS_EOM( tau, KS_t_eps, a, mu ) 
+function KS_t_eps_prime = KS_EOM( tau, KS_t_eps, a_VTN, mu ) 
 % Integrate equations of motion for u, uprime, t, and energy 
 
     % get u and uprime (tau velocity)  
     u        = KS_t_eps(1:4) ; 
     u_prime  = KS_t_eps(5:8) ; 
     
-    % a is in VTN frame. Transform into u basis ? 
-        
+    % a is in VTN frame. Transform into inertial, then u basis ? 
+    % first get r, v, and a stuff ... 
+    rv  = KS2rv( [ u; u_prime ] ) ; 
+    r   = rv(1:3) ; v = rv(4:6) ; 
+    
+    % get vectors you need 
+    r_hat = r / norm(r) ; v_hat = v / norm(v) ; 
+    h     = cross(r, v) ; h_hat = h / norm(h) ; 
+    T     = cross(h, v) ; T_hat = T / norm(T) ; 
+    
+    % a_VTN --> a_Cartesian 
+    a_V   = a_VTN(1) ; a_T = a_VTN(2) ; a_N = a_VTN(3) ; 
+    a_XYZ = a_V * v_hat + a_T * ( cross(h_hat, v_hat) ) + a_N * h_hat ; 
+    a_XYZ = [ a_XYZ ; 0 ] ; 
+    
     % get uprimeprime (tau acceleration) 
     r               = dot( u, u ) ; 
     eps             = 2 * dot( u_prime, u_prime ) / r - mu / r ;     
-    u_prime_prime   = eps/2 * u + 1/2 * r * Lu_fn(u)' * a ; 
+    u_prime_prime   = eps/2 * u + 1/2 * r * Lu_fn(u)' * a_XYZ ; 
     
     % time and energy derivatives wrt tau 
     t_prime   = r ;     
-    eps_prime = 2 * dot(Lu_fn(u)' * a, u_prime ) ; 
+    eps_prime = 2 * dot(Lu_fn(u)' * a_XYZ, u_prime ) ; 
 
     % output 
     KS_t_eps_prime = [ u_prime ; u_prime_prime ; t_prime ; eps_prime ] ; 
