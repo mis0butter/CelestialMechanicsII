c9d860d3715f85ee602627e7afc1da9efcf5dea8
mis0butter <junetter@gmail.com>
2024-04-19 17:27:32 -0500

    ok, getting out the hamiltonian plot. It doesn't match exactly the paper plots, but it looks pretty good I think

--- StarkProject/stark_main.m
+++ StarkProject/stark_main.m
@@ -31,21 +31,25 @@ caseSun = 2 ;
 n = sqrt( mu / a^3 ) ; 
 
 % 0: alpha = 0, tau = time 
-if casesun == 0 
+if caseSun == 0 
     tau_p = 2 * pi / ( n * cSun ) ; 
+    alphaSun = 0 ; 
 
 % 1: alpha = 1, tau = eccentric anomaly 
 elseif caseSun == 1 
     tau_p = 2 * pi / ( n * cSun * a ) ;  
+    alphaSun = 1 ; 
 
 % 2: alpha = 2, tau = true anomaly 
 elseif caseSun == 2 
     tau_p = 2 * pi / ( n * cSun * sqrt( a * ( 1 - e^2 ) ) ) ; 
+    alphaSun = 2 ; 
 
 % 3: alpha = 3/2, tau = intermediate anomaly 
 elseif caseSun == 3 
     M     = sqrt( 2 * e / ( 1 + e ) ) ; 
     tau_p = 4 * K( M ) / ( cSun * sqrt( mu * ( 1 + e ) ) ) ; 
+    alphaSun = 3/2 ; 
 
 % 4: alpha = alphaSun, generic Sundman transformation: user provided alpha  
 else 
@@ -59,7 +63,7 @@ N    = 50 ;
 dtau = tau_p / N ; 
 
 % order: order of the TS integration 
-order = 4 ; 
+order = 8 ; 
 
 % Xf: Integrated vector 
 Xf = zeros(7,1) ; 
@@ -98,11 +102,36 @@ disp('')
     cSun, caseSun, alphaSun, caseStark, divSafeguard, ... 
     accuracyFlag, Xf, accuracyOut, statusFlag ) ; 
 
-%% display checks 
+% propagate N dtau steps 
+Xk   = X0 ; 
+Xkp1 = Xf ; 
+X_hist = [ Xk' ] ; 
+accuracy_hist = [ accuracyOut ] ; 
+for i = 1 : N 
+    
+    % propagate one dtau step 
+    [ Xkp1, accuracyOut, statusFlag ] = FGStark_oneStep_propagator( Xk, acc, dtau, order, ... 
+        cSun, caseSun, alphaSun, caseStark, divSafeguard, ... 
+        accuracyFlag, Xkp1, accuracyOut, statusFlag ) ;     
+    
+    % save hist 
+    X_hist = [ X_hist ; Xkp1' ] ; 
+    accuracy_hist = [ accuracy_hist ; accuracyOut ] ; 
+    
+    % ready for next iter 
+    Xk = Xkp1 ; 
+    
+end 
+
+%% display checks and plot 
 
 disp('Status: ' ) ; disp(statusFlag) ; 
 disp('Xf: ') ; disp(Xf) 
 disp('del Hamiltonian:') ; disp(accuracyOut(1)) ; 
+
+figure() ; hold on ; grid on ; 
+    semilogy( accuracy_hist(:,1) ) ; 
+    title( sprintf( ' alpha = %d', alphaSun ) ) ; 
 
         
 
