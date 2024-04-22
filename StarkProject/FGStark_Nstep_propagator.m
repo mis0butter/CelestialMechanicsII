2ff91695d4f3211a24e748494f31d8e8d11d1f70
Junette Hsin <junetter@gmail.com>
2024-04-22 15:05:54 -0500

    committing code that works for propagating N tau periods or N steps

--- StarkProject/FGStark_Nstep_propagator.m
+++ StarkProject/FGStark_Nstep_propagator.m
@@ -2,44 +2,43 @@ function [ X_hist, accuracy_hist, status_hist, acc_hist ] = FGStark_Nstep_propag
         cSun, caseSun, alphaSun, caseStark, divSafeguard, ... 
         accuracyFlag, Xf, accuracyOut, statusFlag ) 
 
-% dealing with acceleration  
-acc_norm = norm(acc) ; 
-q = 10^(-3) ; 
-    
-Xk   = X0 ; 
-Xkp1 = Xf ; 
-X_hist        = [ Xk ] ; 
-accuracy_hist = [ accuracyOut ] ; 
-status_hist   = [ statusFlag ] ; 
-acc_hist      = [  ] ; 
+    % dealing with acceleration  
+    acc_norm = norm(acc) ; 
+    q = 10^(-3) ; 
 
-for i = 1 : N 
-    
-    % rv to oe 
-    oe_k = rv2oe( Xk(1:6), 1 ) ; 
-    nu_k = oe_k(6) ; 
-    r_norm = norm( Xk(1:3) ) ;
-    
-    % orient acc in same direction as velocity 
-    v_vec = Xk(4:6) ; 
-    v_hat = v_vec / norm( v_vec ) ; 
-%     acc   = acc_norm * v_hat ; 
-    acc = q / r_norm * ( 1 + cos(nu_k) ) * v_vec ;  
-    
-    % propagate one dtau step 
-    [ Xkp1, accuracyOut, statusFlag ] = FGStark_oneStep_propagator( Xk, acc, dtau, order, ... 
-        cSun, caseSun, alphaSun, caseStark, divSafeguard, ... 
-        accuracyFlag, Xkp1, accuracyOut, statusFlag ) ;     
-    
-    % save hist 
-    X_hist        = [ X_hist ; Xkp1 ] ; 
-    accuracy_hist = [ accuracy_hist ; accuracyOut ] ; 
-    status_hist   = [ status_hist ; statusFlag ] ; 
-    acc_hist      = [ acc_hist ; norm(acc) ] ; 
-    
-    % ready for next iter 
-    Xk = Xkp1 ; 
-    
-end 
+    Xk   = X0 ; 
+    Xkp1 = Xf ; 
+    X_hist        = [ Xk ] ; 
+    accuracy_hist = [ accuracyOut ] ; 
+    status_hist   = [ statusFlag ] ; 
+    acc_hist      = [  ] ; 
+
+    for i = 1 : N 
+
+        % rv to oe 
+        oe_k = rv2oe( Xk(1:6), 1 ) ; 
+        nu_k = oe_k(6) ; 
+        r_norm = norm( Xk(1:3) ) ;
+
+        % orient acc in same direction as velocity 
+        v_vec = Xk(4:6) ; 
+        acc   = q / r_norm * ( 1 + cos(nu_k) ) * v_vec ;  
+        acc   = 0 * acc ; 
+
+        % propagate one dtau step 
+        [ Xkp1, accuracyOut, statusFlag ] = FGStark_oneStep_propagator( Xk, acc, dtau, order, ... 
+            cSun, caseSun, alphaSun, caseStark, divSafeguard, ... 
+            accuracyFlag, Xkp1, accuracyOut, statusFlag ) ;     
+
+        % save hist 
+        X_hist        = [ X_hist ; Xkp1 ] ; 
+        accuracy_hist = [ accuracy_hist ; accuracyOut ] ; 
+        status_hist   = [ status_hist ; statusFlag ] ; 
+        acc_hist      = [ acc_hist ; norm(acc) ] ; 
+
+        % ready for next iter 
+        Xk = Xkp1 ; 
+
+    end 
 
 end 
